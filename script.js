eed35eeca02703f3bfe0f23da37a61b53cbc14c9
Kushal Vardhan <vardhank140102@gmail.com>
2024-09-17 19:53:07 +0530

    small project

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const clock = document.getElementById('clock');
-let date = new Date();
-console.log(date.toLocaleTimeString())
+setInterval(function(){
+    let date = new Date();
+// console.log(date.toLocaleTimeString())
+clock.innerHTML = date.toLocaleTimeString();
+},1000)
